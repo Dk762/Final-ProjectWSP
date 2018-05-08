d1b7a7c4823970948ddf2ab4b093c3bcd1370392
Unknown <pateldhaval762@gmail.com>
2018-05-08 17:49:44 -0400

    top slider added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { CarouselModule } from 'ng-bootstrap/carousel';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -11,6 +13,8 @@ import { HomeComponent } from './home/home.component';
 import { RoutineComponent } from './routine/routine.component';
 import { LoginComponent } from './login/login.component';
 import { RoutineService } from './services/routine.service';
+import { CarouselConfig } from 'ng-bootstrap/carousel/carousel.config';
+import { TopsliderComponent } from './topslider/topslider.component';
 
 
 
@@ -22,6 +26,7 @@ import { RoutineService } from './services/routine.service';
     HomeComponent,
     RoutineComponent,
     LoginComponent,
+    TopsliderComponent,
     
   ],
   imports: [
@@ -33,7 +38,9 @@ import { RoutineService } from './services/routine.service';
       {path: 'routine', component: RoutineComponent },
       {path: 'login', component: LoginComponent },
       {path: '', redirectTo: '/home', pathMatch: 'full'}
-    ])
+    ]),
+    CarouselModule.forRoot(),
+    NgbModule.forRoot()
   ],
   providers: [RoutineService],
   bootstrap: [AppComponent]
