7195e057780c1b557ef730822df5b463fcd501fe
Unknown <pateldhaval762@gmail.com>
2018-05-11 19:51:18 -0400

    add midleware

--- server/index.js
+++ server/index.js
@@ -1,19 +1,29 @@
-const express = require('express');
+const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const fitness = require('./fitness/controller');
+const simple = require('./simplecontroller');
+
 var app = express();
 
 const servername = "localhost";
 const port = 8080;
 
-const fitness = require('./fitness/controller');
-const simple = require('./simplecontroller');
+
 app
+        .use(bodyParser.json())
+        .use(bodyParser.urlencoded({ extended: false }))
         .use('/', (req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "*");
             next();
         })
+        .use('/',express.static(path.join(__dirname,"../dist/")))
         .use('/simple', simple)
         .use('/fitness', fitness)
+        .use('/', (req, res, next) => {
+            res.sendFile(path.join(__dirname, "../dist/index.html"));
+        })
         .listen(port);
-
 console.log("Running on http://" + servername + ":" + port);
